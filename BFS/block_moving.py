cf18e98e772e14bc6e76135eb7bd41838f2962cd
JungRyung <soondori92@gmail.com>
2021-06-15 13:38:48 +0900

    update quiz

--- BFS/block_moving.py
+++ BFS/block_moving.py
@@ -1,8 +1,13 @@
 from collections import deque
 dx = [-1,0,1,0]
 dy = [0,-1,0,1]
-def get_next_pos(tmp_map,pos1,pos2):
+def get_next_pos(tmp_map,pos):
     next_pos = []
+    pos = list(pos)
+    tmp_map[pos[0][0]][pos[0][1]] = 2
+    tmp_map[pos[1][0]][pos[1][1]] = 2
+    pos1 = pos[0]
+    pos2 = pos[1]
     # 상하 좌우로 이동
     for i in range(4):
         nx1 = pos1[0] + dx[i]
@@ -10,36 +15,35 @@ def get_next_pos(tmp_map,pos1,pos2):
         nx2 = pos2[0] + dx[i]
         ny2 = pos2[1] + dy[i]
         if tmp_map[nx1][ny1]!=1 and tmp_map[nx2][ny2]!=1:
-            next_pos.append(((nx1,ny1),(nx2,ny2)))
+            next_pos.append({(nx1,ny1),(nx2,ny2)})
             # 둘다 비워져 있는 경우 -> 회전이 가능한 경우
             if tmp_map[nx1][ny1]==0 and tmp_map[nx2][ny2]==0:
-                next_pos.append(((pos1[0],pos1[1]),(nx1,ny1)))
-                next_pos.append(((pos2[0],pos2[1]),(nx2,ny2)))
+                next_pos.append({(pos1[0],pos1[1]),(nx1,ny1)})
+                next_pos.append({(pos2[0],pos2[1]),(nx2,ny2)})
     return next_pos
     
 def solution(board):
     n = len(board)
-    board.insert(0,[1]*(n+2))
-    board.append([1]*(n+2))
+    board.insert(0,[1]*n)
+    board.append([1]*n)
     for i in range(n+2):
         board[i].insert(0,1)
         board[i].append(1)
     
-    tmp_pos1 = (1,1)
-    tmp_pos2 = (1,2)
-    board[1][1] = 2
-    board[1][2] = 2
+    pos = {(1,1),(1,2)}
     q = deque()
     visit = []
-    q.append((tmp_pos1,tmp_pos2))
-    visit.append(tmp_pos1)
-    visit.append(tmp_pos2)
+    q.append((pos,0))
+    visit.append(pos)
     while q:
-        tmp_pos1, tmp_pos2 = q.popleft()
-        for pos1, pos2 in get_next_pos(board,tmp_pos1,tmp_pos2):
-            if 
-        
-    return answer
+        pos, cost = q.popleft()
+        if (n,n) in pos:
+            return cost
+        for next_pos in get_next_pos(board,pos):
+            if next_pos not in visit:
+                q.append((next_pos,cost+1))
+                visit.append(next_pos)
+    return 0
 
 board = [[0,0,0,1,1],[0,0,0,1,0],[0,1,0,1,1],[1,1,0,0,1],[0,0,0,0,0]]
 answer = solution(board)
