eef6a1fc17cce2b7a92c12784ce2e361d8dd56cc
JungRyung <soondori92@gmail.com>
2021-07-02 21:42:19 +0900

    update quiz

--- dynamic_programming/RGB_street2.py
+++ dynamic_programming/RGB_street2.py
@@ -8,16 +8,10 @@ costs = [[] for _ in range(n)]
 for i in range(n):
     costs[i] = list(map(int, sys.stdin.readline().split()))
 
-cost = [0]*3
-for i in range(3):
-    before = i
-    cost[i] = costs[0][i]
-    for j in range(1,n):
-        min_cost = 1001
-        for k in range(3):
-            if before!= k:
-                min_cost = min(min_cost,costs[j][k])
-                tmp = k
-        cost[i] += min_cost
-        before = tmp
-print(min(cost))
+da = [[0]*3 for _ in range(n)]
+da[0] = costs[0]
+for i in range(1,n):
+    da[i][0] = min(da[i-1][1], da[i-1][2]) + costs[i][0]
+    da[i][1] = min(da[i-1][0], da[i-1][2]) + costs[i][1]
+    da[i][2] = min(da[i-1][0], da[i-1][1]) + costs[i][2]
+print(min(da[n-1]))
