d51bf848ef8fdec6b771320b67ffd48e10416483
JungRyung <soondori92@gmail.com>
2021-07-14 14:25:52 +0900

    sole tree

---
+++ tree/phone_number_list.cpp
@@ -0,0 +1,66 @@
+/*
+TITLE   : 전화번호 목록
+URL     : https://www.acmicpc.net/problem/5052
+DATE    : 21.07.14
+*/
+#include <iostream>
+#include <string>
+#include <string.h>
+
+using namespace std;
+
+struct Node{
+    bool is_end;
+    Node *next[10];
+    Node(){
+        is_end = false;
+        memset(next,0,sizeof(next));
+    }
+};
+
+class Dictree{
+private:
+    Node *root;
+public:
+    Dictree(){
+        this->root = new Node;
+    }
+    bool insert(Node *curr,char* num){
+        if(curr->is_end)
+            return false;
+        if(*num == '\n'){
+            curr->is_end = true;
+            return true;
+        }
+        else{
+            int curr_num = *num - '0';
+            if(curr->next[curr_num]==nullptr)
+                curr->next[curr_num] = new Node;
+            return this->insert(curr->next[curr_num], num + 1);
+        }
+    }
+    Node* get_root(){
+        return this->root;
+    }
+};
+
+int main(){
+    int t=0;
+    scanf("%d",&t);
+    while(t--){
+        int n=0;
+        scanf("%d",&n);
+        Dictree tree;
+        for(int i=0; i<n; i++){
+            char* s;
+            scanf("%s",s);
+            bool valid = tree.insert(tree.get_root(),s);
+            if(valid)
+                printf("valid!\n");
+            else
+                printf("not valid!\n");
+        }
+    }
+
+    return 0;
+}
--- tree/tree.cpp
+++ tree/tree.cpp
@@ -5,66 +5,51 @@ DATE    : 21.07.13
 */
 #include <iostream>
 #include <vector>
-#include <stack>
 #include <algorithm>
-#define MAX_LEN 51
+#define MAX_LEN 55
 
 using namespace std;
 
 vector<int> tree[MAX_LEN];
 bool visit[MAX_LEN];
+int cnt;
+
+bool dfs(int curr, int removed){
+    if(curr == removed)
+        return false;
+    int leng = tree[curr].size();
+    bool searched = false;
+    for(int i=0; i<leng; i++){
+        int next = tree[curr][i];
+        if(!visit[next]){
+            bool valid = dfs(next,removed);
+            if(valid){
+                searched = true;
+            }
+        }
+    }
+    if(!searched)
+        cnt++;
+    return true;
+}
 
 int main(){
-    int n;
-    int root;
+    int n=0;
+    int root=0;
     scanf("%d",&n);
     for(int i=0; i<n; i++){
         int parent;
         scanf("%d",&parent);
-        tree[parent].push_back(i);
         if(parent==-1)
             root = i;
+        else
+            tree[parent].push_back(i);
+        
     }
     int removed_node;
     scanf("%d",&removed_node);
-
-    vector<int>().swap(tree[removed_node]);
-    for(int i=0; i<n; i++){
-        auto it = find(tree[i].begin(), tree[i].end(), removed_node);
-        if(it != tree[i].end())
-            tree[i].erase(it);
-    }
     
-    // dfs
-    stack<int> s;
-    s.push(root);
-    int cnt = 0;
-    while(!s.empty()){
-        int curr = s.top();
-        bool first_time = false;
-        if(!visit[curr]){
-            first_time = true;
-            visit[curr] = true;
-        }
-        int leng = tree[curr].size();
-        bool searched = false;
-        for(int i=0; i<leng; i++){
-            int next = tree[curr][i];
-            if(!visit[next]){
-                visit[next] = true;
-                s.push(next);
-                searched = true;
-                break;
-            }
-        }
-        if(first_time && !searched){
-            cnt++;
-            s.pop();
-        }else if(!searched){
-            s.pop();
-        }
-    }
-    
+    dfs(root, removed_node);
     printf("%d\n",cnt);
 
     return 0;
