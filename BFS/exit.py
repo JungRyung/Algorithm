dcf43d33de03a3d88c2f8ae5ce3244d82c3a699c
JungRyung <soondori92@gmail.com>
2021-08-05 19:00:10 +0900

    solve exit

--- BFS/exit.py
+++ BFS/exit.py
@@ -19,14 +19,49 @@ for i in range(r):
         if forest[i][j] == 'S':
             start_x, start_y = i, j
 
+# def print_map():
+#     for i in range(r):
+#         for j in range(c):
+#             print(forest[i][j], end=' ')
+#         print()
+
 def flow_water():
     visit = [[False]*c for _ in range(r)]
     for i in range(r):
         for j in range(c):
-            if not visit[i][j]:
+            if not visit[i][j] and forest[i][j] == '*':
                 visit[i][j] = True
                 for k in range(4):
                     nx = i + dx[k]
                     ny = j + dy[k]
                     if 0<=nx<r and 0<=ny<c and not visit[nx][ny] and forest[nx][ny] == '.':
-                        
+                        visit[nx][ny] = True
+                        forest[nx][ny] = '*'
+
+def bfs(start_x, start_y):
+    visit = [[False]*c for _ in range(r)]
+    visit[start_x][start_y] = True
+    q = []
+    q.append((start_x, start_y,0))
+    before_t = -1
+    while q:
+        curr_x, curr_y, t = q.pop(0)
+        if before_t != t:
+            flow_water()
+            # print_map()
+        before_t = t
+        for i in range(4):
+            nx = curr_x + dx[i]
+            ny = curr_y + dy[i]
+            if 0<=nx<r and 0<=ny<c and not visit[nx][ny] and (forest[nx][ny] != '*' and forest[nx][ny] != 'X'):
+                if forest[nx][ny] == 'D':
+                    return t+1
+                visit[nx][ny] = True
+                q.append((nx,ny,t+1))
+    return False
+
+t = bfs(start_x, start_y)
+if  t == False:
+    print("KAKTUS")
+else:
+    print(t)
