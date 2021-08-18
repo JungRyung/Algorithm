855ad3149287d35b74769348d65f3864d5021812
JungRyung <soondori92@gmail.com>
2021-08-18 23:04:30 +0900

    solve

--- BFS/laboratory3.py
+++ BFS/laboratory3.py
@@ -4,8 +4,17 @@ URL     : https://www.acmicpc.net/problem/17142
 DATE    : 21.08.17
 '''
 import sys
+from collections import deque
+from itertools import combinations
 
 n, m = map(int, sys.stdin.readline().split())
-lab = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-
-print(lab)
+lab = [[] for _ in range(n)]
+virus = []
+q = deque()
+for i in range(n):
+    lab[i] = list(map(int, sys.stdin.readline().split()))
+    for j in range(n):
+        if lab[i][j] == 2:
+            virus.append((i,j))
+for comb in combinations(virus, 2):
+    
