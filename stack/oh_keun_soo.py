7e0fccc32642526d010da8fb5a8d8ed3bf0afa0c
JungRyung <soondori92@gmail.com>
2021-10-11 20:01:39 +0900

    solves

--- stack/oh_keun_soo.py
+++ stack/oh_keun_soo.py
@@ -4,28 +4,15 @@ URL     : https://www.acmicpc.net/problem/17298
 DATE    : 21.10.09
 '''
 import sys
-from collections import deque
 
 n = int(sys.stdin.readline())
-nums = deque(map(int, sys.stdin.readline().split()))
+nums = list(map(int, sys.stdin.readline().split()))
 
 s = []
-ans = []
-for _ in range(n):
-    curr = nums.popleft()
-    depth = 0
-    ohkeunsoo = -1
-    while nums:
-        next = nums.popleft()
-        if next > curr:
-            ohkeunsoo = next
-            nums.appendleft(next)
-            break
-        else:
-            s.append(next)
-            depth += 1
-    for __ in range(depth):
-        nums.appendleft(s.pop())
-    print(ohkeunsoo,end=' ')
-    
-print()
+s.append(0)
+ans = ['-1'] * n
+for i in range(1,n):
+    while s and nums[s[-1]] < nums[i]:
+        ans[s.pop()] = str(nums[i])
+    s.append(i)
+print(' '.join(ans))
