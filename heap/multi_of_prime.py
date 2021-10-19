863e9f41ff3a9f12a62082e571db2856c68a50ac
JungRyung <soondori92@gmail.com>
2021-10-19 17:57:50 +0900

    solve

---
+++ heap/multi_of_prime.py
@@ -0,0 +1,10 @@
+'''
+TITLE   : 소수의 곱
+URL     : https://www.acmicpc.net/problem/2014
+DATE    : 21.10.19
+'''
+import sys
+
+k, n = map(int, sys.stdin.readline().split())
+primes = list(map(int, sys.stdin.readline().split()))
+
