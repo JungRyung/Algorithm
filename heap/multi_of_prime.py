c9cfb4f0b1f0ee19e177f2258291dbca8838a521
JungRyung <soondori92@gmail.com>
2021-10-20 21:23:30 +0900

    solve

--- heap/multi_of_prime.py
+++ heap/multi_of_prime.py
@@ -4,7 +4,17 @@ URL     : https://www.acmicpc.net/problem/2014
 DATE    : 21.10.19
 '''
 import sys
+import heapq
 
 k, n = map(int, sys.stdin.readline().split())
 primes = list(map(int, sys.stdin.readline().split()))
 
+ans = list(primes)
+heapq.heapify(ans)
+for _ in range(n):
+    front = heapq.heappop(ans)
+    for p in primes:
+        heapq.heappush(ans, front*p)
+        if front % p == 0:
+            break
+print(front)
