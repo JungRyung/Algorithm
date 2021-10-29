3b9a400339f4e7b60505949e215bd64b4331d767
JungRyung <soondori92@gmail.com>
2021-10-29 19:58:24 +0900

    solve

--- BFS/monkey_who_want_to_be_horse.py
+++ BFS/monkey_who_want_to_be_horse.py
@@ -17,8 +17,8 @@ land = [list(map(int, sys.stdin.readline().split())) for _ in range(h)]
 
 
 def bfs():
-    visit = [[False]*w for _ in range(h)]
-    visit[0][0] = True
+    # visit = [[False]*w for _ in range(h)]
+    # visit[0][0] = True
     q = deque()
     q.append((0,0,0,0))
 
@@ -27,17 +27,16 @@ def bfs():
         for i in range(12):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0<=nx<h and 0<=ny<w and not visit[nx][ny] and land[nx][ny] == 0:
+            if 0<=nx<h and 0<=ny<w and land[nx][ny] == 0:
                 # 앞뒤양옆으로 이동할 때
                 if 0<=i<4:
                     if nx==h-1 and ny==w-1:
                         return t+1
-                    visit[nx][ny] = True
                     q.append((nx,ny,t+1,jump))
+                # 눈 목 자로 이동할 때
                 elif 4<=i<12 and jump<k:
                     if nx==h-1 and ny==w-1:
                         return t+1
-                    visit[nx][ny] = True
                     q.append((nx,ny,t+1,jump+1))
     return -1
 ans = bfs()
