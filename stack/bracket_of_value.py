0d4de6428d517a8933902036058f735f8d283d86
JungRyung <soondori92@gmail.com>
2021-11-19 22:15:16 +0900

    solving

---
+++ stack/bracket_of_value.py
@@ -0,0 +1,12 @@
+'''
+TITLE   : 괄호의 값
+URL     : https://www.acmicpc.net/problem/2504
+DATE    : 21.11.18
+'''
+import sys
+
+brackets = list(sys.stdin.readline().rstrip())
+print(brackets)
+
+def calculate(bracketlist):
+    
